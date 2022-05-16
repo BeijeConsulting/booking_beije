e505f2ee6ef080c322f05d5716edf3fc602781d5
Paolo Pascucci <pascuccip9@gmail.com>
2022-05-16 15:29:21 +0200

    Add routes login FE

--- src/Routing.js
+++ src/Routing.js
@@ -7,13 +7,26 @@ import { routes } from "./routes/routes";
 //Screens
 import Home from './screens/frontEnd/home/Homepage'
 
+import Login from './screens/frontEnd/login/Login'
+import Bookings from './screens/frontEnd/account/Bookings'
+import Messages from './screens/frontEnd/account/Messages'
+import SingleConversation from './screens/frontEnd/account/SingleConversation'
+import Settings from './screens/frontEnd/account/Settings'
+
+
+
 function Routing() {
   return (
     <Routes>
 
-      {/* all the routes for frontEnd goes inside this one */}
-      <Route path={routes.HOME} element={<Home />}>
+      <Route path={routes.HOME} element={<Home />}></Route>
 
+      {/* all the routes for frontEnd goes inside this one */}
+      <Route path={routes.LOGIN} element={<Login />}>
+        <Route path={routes.BOOKED} element={<Bookings />}></Route>
+        <Route path={routes.MESSAGES} element={<Messages />}></Route>
+        <Route path={routes.SINGLECONVERSATION} element={<SingleConversation />}></Route>
+        <Route path={routes.SETTINGS} element={<Settings />}></Route>
       </Route>
 
 
--- src/screens/frontEnd/home/Homepage.js
+++ src/screens/frontEnd/home/Homepage.js
@@ -1,9 +1,24 @@
 import React from "react";
+// import { navigate } from "../../../utils/Utils";
+import { routes } from "../../../routes/routes";
+
+ import { useNavigate } from "react-router-dom";
+
+
+
 
 const Homepage = () => {
+
+  let vector = useNavigate()
+
+  const nav = () => {
+    vector(routes.LOGIN)
+  }
+
   return (
     <>
       <p>Home</p>
+      <button onClick={nav}>Login</button>
     </>
   );
 };
