4bbba744ed0a8560156ffef1f2916fb33d7c71b0
NicolaL95 <nicola.lilla@gmail.com>
2022-05-16 16:12:30 +0200

    add room components

--- src/Routing.js
+++ src/Routing.js
@@ -5,18 +5,20 @@ import { Routes, Route } from 'react-router-dom';
 import { routes } from "./routes/routes";
 
 //Screens
-import Home from './screens/frontEnd/home/Home'
+import Home from './screens/frontEnd/home/Homepage'
+import Rooms from "./components/frontEnd/funcComponents/rooms/Rooms";
 //Screen backOffice
-import MessageChat from './screens/backOffice/MessageChat/MessageChat'
+/* import MessageChat from './screens/backOffice/MessageChat/MessageChat' */
 import ReservationCalendar from './screens/backOffice/host/ReservationCalendar/ReservationCalendar'
 import HostAccount from "./screens/backOffice/host/HostAccount/HostAccount";
+import PropertyCards from "./components/frontEnd/funcComponents/propertyCards/PropertyCards";
 
 function Routing() {
     return (
         <Routes>
 
             {/* all the routes for frontEnd goes inside this one */}
-            <Route path={routes.HOME} element={<Home />}>
+            <Route path={routes.HOME} element={<Rooms />}>
 
             </Route>
 
@@ -24,7 +26,7 @@ function Routing() {
             {/* all the routes for backOffice goes inside this one */}
             <Route path={routes.DASHBOARD} element={<Home />}>
                 <Route path={routes.HOST_ACCOUNT} element={<HostAccount />} />
-                <Route path={routes.MESSAGE_CHAT} element={<MessageChat />} />
+                {/* <Route path={routes.MESSAGE_CHAT} element={<MessageChat />} /> */}
                 <Route path={routes.RESERVATION_CALENDAR} element={<ReservationCalendar />} />
             </Route>
 
