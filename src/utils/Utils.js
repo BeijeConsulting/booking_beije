43bbf971dd7b9160b88c589f9902d53396bf39b0
Andrea Locatelli <ciao.loca@gmail.com>
2022-05-17 16:32:24 +0200

    Fix utils

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,34 +1,19 @@
 function createPortalNode(idNode) {  // simple function for make an element and add it to the dom
-   const nodeElement = document.createElement('div');
-   
-   nodeElement.setAttribute('id', idNode);
-   document.body.appendChild(nodeElement);
-
-   return nodeElement;
-}
-
-
-<<<<<<< HEAD
-// export {
-//     navigate
-// }
-
-// {
-//     "id": 40,
-//     "email": "a@gmail.com",
-//     "password": "pippo",
-//     "name": "pippo",
-//     "surname": "baudo"
-// }
-=======
-const renderSelectOptions = (item, key) => {
-   return (
-      <option key={`${item}-${key}`} value={item}>{item}</option>
-   )
-}
-
-export {
-   renderSelectOptions,
-   createPortalNode
-}
->>>>>>> 3c3d3cf8d439a45b8aadb40b9005c4308083a895
+    const nodeElement = document.createElement('div');
+    
+    nodeElement.setAttribute('id', idNode);
+    document.body.appendChild(nodeElement);
+ 
+    return nodeElement;
+ }
+ 
+ const renderSelectOptions = (item, key) => {
+    return (
+       <option key={`${item}-${key}`} value={item}>{item}</option>
+    )
+ }
+ 
+ export {
+    renderSelectOptions,
+    createPortalNode
+ }
