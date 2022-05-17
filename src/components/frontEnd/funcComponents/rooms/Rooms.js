3b6a16c768ed17dbc56b0e0ad63b53b500956c4e
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-05-17 15:59:17 +0200

    fix : delete unused fontAwesome icon in import

--- src/components/frontEnd/funcComponents/rooms/Rooms.js
+++ src/components/frontEnd/funcComponents/rooms/Rooms.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Rooms.less';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome/index'
-import { faInfoCircle, faUser, faShower, faFan, faCoffee, faSmoking } from '@fortawesome/free-solid-svg-icons'
+import { faInfoCircle, faUser } from '@fortawesome/free-solid-svg-icons';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import UiButton from '../ui/buttons/uiButtons/UiButton';
