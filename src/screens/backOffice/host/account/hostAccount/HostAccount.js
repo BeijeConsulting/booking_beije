6a4bd54eb08abf7e2b3551f0f426965d278cc0af
Federico Frascà <federico.frasca796@gmail.com>
2022-05-17 17:47:54 +0200

    add tests in hostAccount screen

--- src/screens/backOffice/host/account/hostAccount/HostAccount.js
+++ src/screens/backOffice/host/account/hostAccount/HostAccount.js
@@ -1,23 +1,27 @@
 import { React } from "react";
 import PropTypes from "prop-types";
 
+// STYLE
 import "./HostAccount.less"
 
 // COMPONENTS
 import { Button } from "antd";
+import HorizontalCard from "../../../../../components/backOffice/hookComponents/horizontalCard/HorizontalCard";
+import CardList from "../../../../../components/backOffice/hookComponents/cardList/CardList";
 
 // FontAwesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCrown } from "@fortawesome/free-solid-svg-icons";
-import HorizontalCard from "../../../../../components/backOffice/hookComponents/horizontalCard/HorizontalCard";
-import CardList from "../../../../../components/backOffice/hookComponents/cardList/CardList";
+
+// UTILS
+import { wrapperMap } from "../../../../../utils/generalIteration/generalIteration";
 
 const HostAccount = (props) => {
 
     const cardsTest = [
         {
-            img: "https://picsum.photos/200/300",
-            title: "Camera bellissima",
+            imgSrc: "https://picsum.photos/200/300",
+            accountName: "Camera bellissima",
             text: "Lorem ipsum dolor sit amet li mortacis delo reduxos",
             href: "notarealurl.io",
         },
@@ -47,8 +51,20 @@ const HostAccount = (props) => {
         },
     ];
 
+    const navigateTo = (e) => {
+        console.log("navigato to ...param", e);
+    }
+    const paginationVars = {
+        numberOfItems: 20,
+        currentPage: 2,
+        pageSize: 10,
+        callback: navigateTo
+    }
+
     return <>
         HostAccount screen
+
+        {/* DOWN BELOW ARE TESTS !!!! */}
 
         {/* <HorizontalCard
             imageSrc: "https://picsum.photos/200/300"
@@ -69,12 +85,21 @@ const HostAccount = (props) => {
                 <Button>Delete rooms</Button>
             </>
             }
-            cards={cardsTest}
-            currentPage={1} // currently doesn't work
-            totalPages={5} // currently doesn't work
-
-        />
+            pagination={paginationVars}
+        >
+            {/* {wrapperMap(HorizontalCard, cardsTest)} */}
+            {cardsTest.map(renderCards)}
+        </CardList>
     </>
+}
+
+const renderCards = (card, key) => {
+    return <HorizontalCard
+        imageSrc={card?.img}
+        title={card?.title}
+        text={card?.text}
+        key={key}
+    />
 }
 
 HostAccount.defaultProps = {
