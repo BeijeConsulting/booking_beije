a3b9740945d159e480e929efae23efb67f3c34bd
NicolaL95 <nicola.lilla@gmail.com>
2022-05-18 11:46:23 +0200

    add AnnuncioAPIList

---
+++ src/services/api/annuncio/annuncioApi.js
@@ -0,0 +1,45 @@
+import { getApi, deleteApi, putApi, postApi } from '../../genericServices'
+
+
+
+const annunciGetApi = async () => {
+    return await getApi("annunci");
+}
+
+const annunciAccettazioneGetApi = async () => {
+    return await getApi("annunci/accettazione");
+}
+
+const annuncioDetailGetApi = async (id) => {
+    return await getApi(`annuncio/${id}`);
+}
+
+const annuncioOnStrutturaGetApi = async (id) => {
+    return await getApi(`annuncio/${id}`);
+}
+
+/* AUTH:HOST */
+const annuncioModifyPutApi = async (id, obj, header) => {
+    return await putApi(`annuncio/${id}`, obj, header);
+}
+
+/* AUTH:HOST */
+const annuncioInsertMutiplePostApi = async (quantity, obj, header) => {
+    return await postApi(`annuncio/${quantity}`, obj, header);
+}
+/* AUTH:ADMIN */
+const annuncioDeleteApi = async (id, header) => {
+    return await deleteApi(`annuncio/${id}`, header);
+}
+/* AUTH:ADMIN */
+const annuncioapprovePutAPi = async (id, obj, header) => {
+    return await putApi(`annuncio/${id}`, obj, header);
+}
+/* AUTH:ADMIN */
+const annuncioDeclinePutApi = async (id, obj, header) => {
+    return await putApi(`annuncio/${id}`, obj, header);
+}
+
+
+export { annunciGetApi, annuncioDetailGetApi, annunciAccettazioneGetApi, annuncioModifyPutApi, annuncioDeleteApi, annuncioInsertMutiplePostApi, annuncioapprovePutAPi, annuncioDeclinePutApi, annuncioOnStrutturaGetApi };
+
