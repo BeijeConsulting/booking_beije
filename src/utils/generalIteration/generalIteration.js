dab1a0a225edcf7295fd3fefabbaf0b319fc4e4a
Federico Frascà <federico.frasca796@gmail.com>
2022-05-18 15:13:50 +0200

    improve reusability of wrapperMap
    added custom callback (defined outside array to cycle through)

--- src/utils/generalIteration/generalIteration.js
+++ src/utils/generalIteration/generalIteration.js
@@ -6,21 +6,22 @@ import React, { ReactElement } from "react"
  * toString(36) convert the number in base36(ASCII standard)
  * slice() take the string from index(2) and index(9)
  */
-function randomKey(){
+function randomKey() {
     return Math.random().toString(36).slice(2, 10);
 
 }
 /**
  * Function map custom. Render the component given as parameters
- * @param  {ReactElement} Component
- * @param  {array} array
- * @returns {ReactElement} return the array of the input parameters component
+ * @param {ReactElement} Component React component you want to cycle through array
+ * @param {array} array array to cycle with.
+ * @param {Function} callback [optional] a callback function
+ * @returns {ReactElement} returns given React Component cycled through the given array
  */
-const wrapperMap = (Component, array) => {  
+const wrapperMap = (Component, array, callback = null) => {
 
     const renderComponent = (item, key) => {
         return (
-            <Component key={`${key}-${randomKey()}`} item={item} />
+            <Component key={`${key}-${randomKey()}`} item={item} callback={callback} />
         )
     }
     return array.map(renderComponent)
@@ -40,7 +41,7 @@ const wrapperMap = (Component, array) => {
  * @returns {ReactElement} return the array of the input parameters component filtered by trueStatement condition
  */
 const wrapperFilter = (Component, array, trueStatement) => {
-   
+
     let filtered = array.filter(trueStatement)
     return wrapperMap(Component, filtered)
 }
