0b2d14864ef409b146b83bddcfb55c9cdba5a2c8
Paolo <pascuccip9@gmail.com>
2022-05-18 12:54:52 +0200

    Add login css

--- src/components/frontEnd/hookComponents/loginForm/LoginForm.js
+++ src/components/frontEnd/hookComponents/loginForm/LoginForm.js
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 import { routes } from '../../../../routes/routes';
 
 import {postApi} from '../../../../services/genericServices';
-
+import './LoginForm.less'
 
 let formObject = {
    email: '',
@@ -44,15 +44,15 @@ function LoginForm() {
       <section className="container color flex column space">
 
          <div className="flex center column">
-            <div className="w">LOGO</div>
+            <div className="w logo">LOGO</div>
             {/* <Logo></Logo> */}
          </div>
 
          <form>
-            <h1 className="w">Login</h1>
+            <h1 className="w title">Login</h1>
             <div className="formInput flex center column">
-               <FormInput placeholder="Email" info="email" callback={handleChange('email')} />
-               <FormInput placeholder="Password" info="password" callback={handleChange('password')} />
+               <FormInput className='formInput' placeholder="Email" info="email" callback={handleChange('email')} />
+               <FormInput className='formInput' placeholder="Password" info="password" callback={handleChange('password')} />
                <br />
                <FormButton className="btn-primary" label="Log in" callback={handleSubmit} />
                <span className="w">or</span>
