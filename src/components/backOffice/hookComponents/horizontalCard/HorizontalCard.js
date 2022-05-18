812eca96ee170d3116a4aa5f82fed7296dc593a2
Federico Frascà <federico.frasca796@gmail.com>
2022-05-18 09:12:20 +0200

    refactor horizontalCard

--- src/components/backOffice/hookComponents/horizontalCard/HorizontalCard.js
+++ src/components/backOffice/hookComponents/horizontalCard/HorizontalCard.js
@@ -1,14 +1,8 @@
 import { React } from "react";
 import PropTypes from "prop-types";
 
+//STYLE
 import "./HorizontalCard.less"
-
-// AntDesign COMPONENTS
-import { Button } from "antd";
-
-// FontAwesome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCrown } from "@fortawesome/free-solid-svg-icons";
 
 const HorizontalCard = (props) => {
 
@@ -36,15 +30,13 @@ const HorizontalCard = (props) => {
 
             <div className={"card_body"}>
                 <div className={"card_main"}>
-
                     <div className={"left_col"}>
-
                         <h5 className={"card_title"}>
                             {title}
                         </h5>
 
+                        {/* text overflows in mobile viewport. It should be truncated in CSS*/}
                         <p className="card_text">
-                            {/* text overflows in mobile viewport. It should be truncated in CSS*/}
                             {text}
                         </p>
                     </div>
