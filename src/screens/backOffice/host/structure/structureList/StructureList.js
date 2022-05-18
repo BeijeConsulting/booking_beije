e6f4ed90527f379f540daa63ba0874c37909adec
murdock00 <alessandro.murdocca@gmail.com>
2022-05-18 12:26:56 +0200

    edit StructureList with upgrade CardList

--- src/screens/backOffice/host/structure/structureList/StructureList.js
+++ src/screens/backOffice/host/structure/structureList/StructureList.js
@@ -1,18 +1,23 @@
 //STYLE
 import "./StructureList.less"
 
+//TRANSLATION
+import { useTranslation } from "react-i18next";
+
 //COMPONENTS
 import CardList from "../../../../../components/backOffice/hookComponents/cardList/CardList";
 import { Button } from "antd";
+import HorizontalCard from "../../../../../components/backOffice/hookComponents/horizontalCard/HorizontalCard";
 
 
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 
 //TODO: manca get da backend
 //TODO: manca navigate che al click sulla singola struttura la passa come oggetto a StructureDetails
 
 const StructureList = () => {
-    const navigate = useNavigate()
+    //const navigate = useNavigate()
+    const { t } = useTranslation()
     const obj = [
         {
             img: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
@@ -31,27 +36,31 @@ const StructureList = () => {
             img: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
             title: "Casa bellissima",
             text: "Casa in riva al mare a Savona"
-        }, {
-            img: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-            title: "Casa bellissima",
-            text: "Casa in riva al mare a Savona"
-        }, {
-            img: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-            title: "Casa bellissima",
-            text: "Casa in riva al mare a Savona"
         },
     ]
 
     const addStructure = (e) => {
-        navigate("add-structure")  /*path for add structure to be put*/
+        /*navigate("add-structure")  path for add structure to be put*/
+    }
+
+    const getCardStructures = (structure, key) => {
+        return <HorizontalCard
+            imageSrc={structure.img}
+            altText={`${key}_${structure.title}`}
+            title={structure.title}
+            text={structure.text}
+        />
     }
 
     return (
 
-        <CardList sectionTitle={"Structure list"}
-            actions={<Button onClick={addStructure} type="primary">Add structure</Button>}
-            cards={obj}
-        />
+        <CardList
+            sectionTitle={t("bo.screens.host.structure.structureListTitle")}
+            actions={<Button onClick={addStructure} type="primary">{t("bo.screens.host.structure.addStructure")}</Button>}
+        >
+            {obj.map(getCardStructures)}
+
+        </CardList>
     )
 }
 
