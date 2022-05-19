5b3d7822bee898e22bed19ad663fbbf92b047b1f
Federico Reghini <federicoreghini@gmail.com>
2022-05-19 21:19:21 +0200

    restore main

--- src/services/config.js
+++
@@ -1,9 +0,0 @@
-//const
-const TIMEOUT = 1000;  // timeout setting in ms
-const BASEURL = 'http://reservation-test.eba-6qvkzxjp.eu-south-1.elasticbeanstalk.com/' //! baseURL for json server. REMEMBER TO SET IT ON PORT 5000! command: json-server --watch "PATH JSON FILE" --port 5000 
-
-
-export {
-    TIMEOUT,
-    BASEURL
-}
