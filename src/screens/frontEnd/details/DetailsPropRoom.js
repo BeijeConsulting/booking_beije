546d9b346e4b0fa6246d2fe149f279e1dadcd542
Federico Reghini <federicoreghini@gmail.com>
2022-05-20 12:58:54 +0200

    file: all screens -> helmet and title

--- src/screens/frontEnd/details/DetailsPropRoom.js
+++ src/screens/frontEnd/details/DetailsPropRoom.js
@@ -1,8 +1,18 @@
 import React from "react";
 
+// modules
+import { Helmet } from "react-helmet";
+import { useTranslation } from "react-i18next";
+
 const DetailsPropRoom = () => {
+
+  const {t} = useTranslation();
+
   return (
     <>
+    <Helmet>
+      <title>{t("fe.screens.propertyDetails.roomDetails")}</title>
+    </Helmet>
       <p>Property Room</p>
     </>
   );
