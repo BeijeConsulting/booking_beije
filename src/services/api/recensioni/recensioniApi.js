00aa855ae82ca074c9769df6aceebe5c622171b3
NicolaL95 <nicola.lilla@gmail.com>
2022-05-20 10:36:26 +0200

    add various api

---
+++ src/services/api/recensioni/recensioniApi.js
@@ -0,0 +1,32 @@
+/* The following API are sorted by AUTH then with the following priority: GET->POST->PUT->PATCH->DELETE  */
+
+import { getApi, deleteApi, putApi, postApi } from '../../genericServices'
+
+
+const reviewsGetApi = async () => {
+    return await getApi(`reviews`);
+}
+
+/* AUTH:USER */
+const reviewIdGetApi = async (id, header) => {
+    return await getApi(`review/${id}`, header);
+}
+
+/* AUTH:USER */
+const reviewPostApi = async (obj, header) => {
+    return await postApi(`review`, obj, header);
+}
+
+/* AUTH:USER */
+const reviewIdModifyPutApi = async (id, obj, header) => {
+    return await putApi(`review/${id}`, obj, header);
+}
+
+/* AUTH:USER */
+const reviewDeleteApi = async (id, header) => {
+    return await deleteApi(`review/${id}`, header);
+}
+
+export {
+    reviewsGetApi, reviewIdGetApi, reviewPostApi, reviewIdModifyPutApi, reviewDeleteApi
+}
