c15c268648cf3f50660ecb1e29eb0d0d01a32718
Federico Reghini <federicoreghini@gmail.com>
2022-05-20 08:29:07 +0200

    fixed

--- src/redux/ducks/userDuck.js
+++ src/redux/ducks/userDuck.js
@@ -1,5 +1,5 @@
 import { myProfilesGetApi } from '../../services/api/user/userApi'
-// import { getLocalStorage } from '../../utils/localStorage/localStorage';
+import { getLocalStorage } from '../../utils/localStorage/localStorage';
 
 const SET_USER = 'web/user/SET_USER';
 const INIT_USER = 'web/user/INIT_USER';
@@ -31,8 +31,7 @@ export default async function userDuck(state = INIT_STATE, action) {
     switch (action.type) {
         case SET_USER:
             newState = Object.assign({}, state);
-            let res = await myProfilesGetApi('eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuaWNvbGFmYXN1bGxpQGdtYWlsLmNvbSIsInJvbGVzIjpbIlVTRVIiXSwiaWF0IjoxNjUyOTY0ODE1LCJleHAiOjE2NTI5Njg0MTV9.mbp0mwQw2OMOwey6YJK43_Y644jVHfWDdmtr44yPKcE');
-            // let res = await myProfilesGetApi(getLocalStorage('token'));
+            let res = await myProfilesGetApi(getLocalStorage('token'));
             newState.user = res.data;
             return newState;
 
