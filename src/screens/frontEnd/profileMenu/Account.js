12872b26c49e1d10e1005700d87442ccc3a6c48a
Paolo <pascuccip9@gmail.com>
2022-05-20 11:01:01 +0200

    Add account css

--- src/screens/frontEnd/profileMenu/Account.js
+++ src/screens/frontEnd/profileMenu/Account.js
@@ -1,10 +1,58 @@
 import React from "react";
 
+
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
+
+import './profileMenuCSS/Account.less'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+
+
 const Account = () => {
   return (
-    <>
-      <p>dashboard</p>
-    </>
+    <div className="Account_container">
+
+      <div className="header_container">
+        <h1>Hi, guest.name</h1>
+        <div>
+          <button className="acc_logout">logout</button>
+        </div>
+      </div>
+
+      <div className="edit">
+        <div>
+          <button className="penIcon"><FontAwesomeIcon icon={faPencil}/></button>
+        </div>
+        <div className="update_cont">
+          <button className="update" type="submit">Update</button>
+        </div>
+      </div>
+
+      <form>
+        <div className="i">
+          <label className="L"><FontAwesomeIcon icon={faPencil}/> Change Name</label>
+          <input type="text"  id="name" />
+        </div>
+        <div className="i">
+          <label  className="L"><FontAwesomeIcon icon={faPencil}/> Change Surname</label>
+          <input type="text"  id="surname" />
+        </div>
+        <div className="i">
+          <label  className="L"><FontAwesomeIcon icon={faPencil}/> Change Mail</label>
+          <input type="email"  id="email" />
+        </div>
+        <div className="i">
+          <label  className="L"><FontAwesomeIcon icon={faPencil}/> Change Password</label>
+          <input type="password"  id="password" />
+        </div>
+        <div className="i">
+          <label className="L" ><FontAwesomeIcon icon={faPencil}/> Confirm Password</label>
+          <input type="password" id="confirmPassword" />
+        </div>
+      </form>
+    </div>
+
   );
 };
 
