1ba1839fc7774af2c54f62e3a808f932638ce3de
Federico Reghini <federicoreghini@gmail.com>
2022-05-21 00:50:00 +0200

    file: searchForm -> added class component

---
+++ src/components/frontEnd/classComponents/pageComponents/searchForm/SearchForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class SearchForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <>
+                SearchForm
+            </>
+        )
+    }
+}
+
+export default SearchForm;
