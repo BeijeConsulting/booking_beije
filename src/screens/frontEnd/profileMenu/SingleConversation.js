79baf6d5d28c7b5806b4059fb22bd1ec77ef9d8b
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-05-23 18:04:15 +0200

    clean singleconversationfile

--- src/screens/frontEnd/profileMenu/SingleConversation.js
+++ src/screens/frontEnd/profileMenu/SingleConversation.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
-import {useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
-// ROUTES 
-import { routes } from '../../../routes/routes'
-
+// get local storage 
+import { getLocalStorage } from "../../../utils/localStorage/localStorage";
 
 //LESS
 import './profileMenuCSS/SingleConversation.less'
 
 //CONNECT
 import { connect } from 'react-redux'
-
+// HELMET 
 import { Helmet } from 'react-helmet'
-// import { messageToSenderIdGetApi } from '../../../services/api/messaggi/messaggiApi'
-// import { getLocalStorage } from "../../../utils/localStorage/localStorage";
+// API 
+import { messageToSenderIdGetApi, messageInsertPostApi } from '../../../services/api/messaggi/messaggiApi'
 import GoBackButton from "../../../components/backOffice/hookComponents/goBackButton/GoBackButton";
 
 
@@ -21,119 +20,44 @@ import { Input } from 'antd';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
-
-
-let inputMessage = null;
-
+let inputMessage;
 const SingleConversation = (props) => {
   const params = useParams();
   const myRef = useRef(null)
-  const inputMessageRef = useRef()
-  const singleConvers =
-  {
-    announceName: 'PizzaPazza',
-    announceProfileIcon: 'https://www.veneto.info/wp-content/uploads/sites/114/chioggia.jpg',
-    hostName: 'Ettore Vettori',
-    messages: [{
-      id: 1,
-      idSender: params.id,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam ipsum, cursus id placerat congue,',
-      dateTime: '2022-05-20'
-    }, {
-      id: 2,
-      idSender: 5,
-      text: 'Lorem ipsum',
-      dateTime: '2022-05-20'
-    }, {
-      id: 3,
-      idSender: 5,
-      text: 'Lorem ipsum 2',
-      dateTime: '2022-05-20'
-    }, {
-      id: 1,
-      idSender: params.id,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam ipsum, cursus id placerat congue,',
-      dateTime: '2022-05-20'
-    }, {
-      id: 1,
-      idSender: 48,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam ipsum, cursus id placerat congue,',
-      dateTime: '2022-05-20'
-    }, {
-      id: 1,
-      idSender: 5,
-      text: 'Lorem ipsum  congue,',
-      dateTime: '2022-05-20'
-    }, {
-      id: 1,
-      idSender: params.id,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam ipsum, cursus id placerat congue,',
-      dateTime: '2022-05-20'
-    },]
-  }
-
-
-
   const [state, setState] = useState({
     msgArray: [],
     windowWidth: window.innerWidth
   })
   useEffect(() => {
-    
     scrollToRef(myRef)
-    function handleResize() {
-      setState({
-        ...state,
-        windowWidth: window.innerWidth
-      })
-    }
     window.addEventListener('resize', handleResize)
     return () => { window.removeEventListener('resize', handleResize) }
   })
   useEffect(() => {
-    if (state.windowWidth < 992) {
-      /*  messageToSenderIdGetApi(params.id, getLocalStorage("token"))
-     .then(res =>{
-       setState({
-         ...state,
-         msgArray: res?.data
-       })
-     }) */
-
-     //da cancellare quando api prontee !
-      setState({
-        ...state,
-        msgArray: singleConvers
-      })
-    } else if (state.windowWidth > 992) {
-      /*  messageToSenderIdGetApi(params.id, getLocalStorage("token"))
-     .then(res =>{
-       setState({
-         ...state,
-         msgArray: res?.data
-       })
-     }) */
-
-     //da cancellare quando api pronte !!!
-      setState({
-        ...state,
-        msgArray:
-        {
-          announceName: 'PizzaPazza',
-          announceProfileIcon: 'https://www.veneto.info/wp-content/uploads/sites/114/chioggia.jpg',
-          hostName: 'Ettore Vettori',
-          messages: [{
-            id: 1,
-            idSender: params.id,
-            text: params.id,
-            dateTime: '2022-05-20'
-          }]
-        }
-      })
-    } 
+    if (localStorage.getItem('token') !== null) {
+      messageToSenderIdGetApi(params.id, getLocalStorage("token"))
+        .then(res => {
+          console.log(res.data)
+          setState({
+            ...state,
+            msgArray: res?.data
+          })
+        })
+    }
   }, [params.id])
+
+  // function to handle window resize 
+  function handleResize() {
+    setState({
+      ...state,
+      windowWidth: window.innerWidth
+    })
+  }
+
   //function to scroll on last messages
   const scrollToRef = (ref) => myRef.current.scrollIntoView({ block: 'end', behavior: 'smooth' })
+
+
   //function to set input value
   const handlerInput = (e) => {
     inputMessage = e.target.value
@@ -142,23 +66,31 @@ const SingleConversation = (props) => {
   // function to submit message on enter press 
   const submitMessageOnEnter = (e) => {
     if (e.key === "Enter") {
-    //chiamata post API
-      // Se risposta ok 
 
       let objcopy = Object.assign({}, state)
       let obj = {
-        idSender: 48,
-        text: inputMessage,
-        dateTime: "2000-22-22"
+        annuncioId: 15,
+        contenuto: inputMessage,
+        receiverId: 22
       }
 
-      objcopy.msgArray.messages.push(obj);
 
-      setState({
-        ...state,
-        msgArray: objcopy.msgArray
-      })
-      
+      objcopy.msgArray.push(obj);
+      // chiamata post API
+      if (localStorage.getItem('token') !== null) {
+        messageInsertPostApi(obj, getLocalStorage('token'))
+          .then(res => {
+            messageToSenderIdGetApi(params.id, getLocalStorage("token"))
+              .then(res => {
+                setState({
+                  ...state,
+                  msgArray: res?.data
+                })
+              })
+          }).catch((e) => {
+            console.log('errore', e)
+          })
+      }
 
     }
 
@@ -166,37 +98,45 @@ const SingleConversation = (props) => {
 
   // function to submit on click in icon 
   const submitMessageOnSendPress = () => {
-    //chiamata post API
-      // Se risposta ok 
     let objcopy = Object.assign({}, state)
     let obj = {
-      idSender: 48,
-      text: inputMessage,
-      dateTime: "2000-22-22"
+      annuncioId: 15,
+      contenuto: inputMessage,
+      receiverId: 22
     }
 
-    objcopy.msgArray.messages.push(obj);
 
-    setState({
-      ...state,
-      msgArray: objcopy.msgArray
-    })
+    objcopy.msgArray.push(obj);
+    // chiamata post API
+    if (localStorage.getItem('token') !== null) {
+      messageInsertPostApi(obj, getLocalStorage('token'))
+        .then(res => {
+          messageToSenderIdGetApi(params.id, getLocalStorage("token"))
+            .then(res => {
+              setState({
+                ...state,
+                msgArray: res?.data
+              })
+            })
+        }).catch((e) => {
+          console.log('errore', e)
+        })
+    }
   }
 
   // render chat 
   function renderConversation(mess, key) {
+
     return (
 
-      <div key={key} className={mess.idSender === params.id ? "conversation conversation-host" : "conversation conversation-guest"}>
+      <div key={key} className={mess.sender.id !== 14 ? "conversation conversation-host" : "conversation conversation-guest"}>
         <div>{
-          mess.idSender === params.id ? singleConvers.announceName : 'You'
+          mess.sender.id !== 14 ? mess.insertion.titolo : 'You'
         }
         </div>
         <p>{mess.text}</p>
-        <div className="dateTimeMessage">{mess.dateTime}</div>
+        <div className="dateTimeMessage">{mess.date_and_time}</div>
       </div>
-
-
     )
   }
 
@@ -212,20 +152,20 @@ const SingleConversation = (props) => {
             <>
               <div className='back-button'><GoBackButton /></div>
 
-              <h1 className='title'>{state?.msgArray?.announceName}</h1>
+              <h1 className='title'>{state?.msgArray[0]?.insertion?.titolo}</h1>
             </>
           }
 
 
           {
-            state?.msgArray?.messages?.map(renderConversation)
+            state?.msgArray.map(renderConversation)
           }
           <span ref={myRef}></span>
         </div>
 
 
         <div className="space-input">
-          <Input ref={inputMessageRef} onKeyPress={submitMessageOnEnter} onChange={handlerInput} className="send_message_input" size="large" placeholder="Write your message..." prefix={<FontAwesomeIcon onClick={submitMessageOnSendPress} className="icon_input_message" icon={faPaperPlane} />} />
+          <Input onKeyPress={submitMessageOnEnter} onChange={handlerInput} className="send_message_input" size="large" placeholder="Write your message..." prefix={<FontAwesomeIcon onClick={submitMessageOnSendPress} className="icon_input_message" icon={faPaperPlane} />} />
         </div>
       </div>
     </>
