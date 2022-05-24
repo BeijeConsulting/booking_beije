a4ec3ded7ac18923d9d4bd1da67ff3ac403ccd0b
Andrea Locatelli <ciao.loca@gmail.com>
2022-05-24 15:16:05 +0200

    CRACO REMOVED

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css'
+import './index.scss'
 
 // react-router-dom
 import { BrowserRouter } from 'react-router-dom';
