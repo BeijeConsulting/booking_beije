a6a559ae1f08f61c7be31632121b02725ee42f21
Andrea Locatelli <ciao.loca@gmail.com>
2022-05-24 14:33:34 +0200

    Removing craco

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.less'
+import './index.css'
 
 // react-router-dom
 import { BrowserRouter } from 'react-router-dom';
