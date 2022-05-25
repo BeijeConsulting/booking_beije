c3bb59663179021f99b937b137070f5552528acb
Federico Reghini <federicoreghini@gmail.com>
2022-05-25 09:16:52 +0200

    non usate useUser

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,17 +1,49 @@
+import { Components } from 'antd/lib/date-picker/generatePicker';
+import { setToken } from '../redux/ducks/tokenDuck';
+import { setUser } from '../redux/ducks/userDuck';
+import { signInPostApi } from '../services/api/auth/authApi';
 import { decryptItem } from '../utils/crypto/crypto';
-import { getLocalStorage, removeLocalStorage } from '../utils/localStorage/localStorage';
+import { getLocalStorage, removeLocalStorage, setLocalStorage } from '../utils/localStorage/localStorage';
 
-export const useUSer = (props) => {
-    if((!getLocalStorage('refreshToken'))){
-        return null
-    }
-    const refreshToken = decryptItem(JSON.parse(localStorage.getItem('refreshToken')));
+export class useUSer extends Components {
 
-    const now = new Date()
-    if ((now.getTime() - refreshToken?.expire) > 14200000) {
-        removeLocalStorage('token');
-        removeLocalStorage('refreshToken');
+
+
+     user = {
+        email: props.userDuck.user.email,
+        password: props.userDuck.user.password
     }
+
+    if((getLocalStorage('refreshToken')) === null) {
+    return null
+}
+
+
+login = () => {
+    if (Object.keys(props.userDuck.user).length !== 0) {
+        signInPostApi(user, getLocalStorage('token')).then(res => {
+            setLocalStorage("token", res?.data?.token);
+            setLocalStorage("refreshToken", res?.data?.refreshToken);
+            props.dispatch(setToken(res?.data?.token));
+            props.dispatch(setUser())
+        })
+    }
+}
+
+const logout = () => {
+    if ((getLocalStorage('refreshToken')) !== null) {
+
+        const refreshToken = decryptItem(JSON.parse(localStorage.getItem('refreshToken')));
+
+        const now = new Date()
+        if ((now.getTime() - refreshToken?.expire) > 14200000) {
+            removeLocalStorage('token');
+            removeLocalStorage('refreshToken');
+        } else {
+            return null;
+        }
+    }
+}
 
     // if ((now.getTime() - refreshToken?.expire) > 14200000) {
     //     const user = decryptItem(JSON.parse(localStorage.getItem('user')));
@@ -46,8 +78,8 @@ export const useUSer = (props) => {
     // }
 }
 
-// const mapStateToProps = (state) => ({
-//     userDuck: state.userDuck
-// })
+const mapStateToProps = (state) => ({
+    userDuck: state.userDuck
+})
 
 connect(mapStateToProps)(useUSer)
