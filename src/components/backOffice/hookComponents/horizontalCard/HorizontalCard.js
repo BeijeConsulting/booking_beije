b25514d368af26e253240caa7843c369c54862e7
Federico Frascà <federico.frasca796@gmail.com>
2022-05-27 00:33:07 +0200

    fix horizontalcard style bug

--- src/components/backOffice/hookComponents/horizontalCard/HorizontalCard.js
+++ src/components/backOffice/hookComponents/horizontalCard/HorizontalCard.js
@@ -29,60 +29,64 @@ const HorizontalCard = (props) => {
     return (
         <div className={"h_card_container"} >
             {/* Card image */}
-            <img className={"card_img"} src={imageSrc} alt={imageAlt} onClick={handleCardClick} />
+            {imageSrc &&
+                <img className={"card_img"} src={imageSrc} alt={imageAlt} onClick={handleCardClick} />
+            }
 
             {/* Card body */}
             <div className={"card_body"}>
                 <div className={"card_main"}>
-                    <h5 className={"card_title"}>
-                        {/* needs refactor */}
-                        <a onClick={handleCardClick} />
-                        {title}
-                    </h5>
-                    {subtitle &&
 
-                        <p className="card_subtitle">{subtitle}</p>
+                    <div className={"left_col"}>
+                        <h5 className={"card_title"}>
+                            <a onClick={handleCardClick} >
+                                {title}
+                            </a>
+                        </h5>
+
+                        {subtitle &&
+                            <p className="card_subtitle">{subtitle}</p>
+                        }
+
+                        {/* text overflows in mobile viewport. It should be truncated in CSS. Needs to be fixed!!*/}
+                        <div className="card_text">
+                            {text}
+                        </div>
+                    </div>
+
+                    {/* Upper right corner (typically used for action buttons or badge-like icons) */}
+                    {upperRightContent &&
+
+                        <aside className={"right_col"}>
+                            {upperRightContent}
+                        </aside>
                     }
-
-                    {/* text overflows in mobile viewport. It should be truncated in CSS. Needs to be fixed!!*/}
-                    <div className="card_text">
-                        {text}
-                    </div>
                 </div>
 
-                {/* Upper right corner (typically used for action buttons or badge-like icons) */}
-                {upperRightContent &&
 
-                    <div className={"right_col"}>
-                        {upperRightContent}
+                {/* Card Footer, typically contains primary/secondary buttons */}
+                {
+                    (footerContentLeft && footerContent) &&
+                    <div className={"card_footer_right_and_left"}>
+                        <div className={"left_col"}>
+                            {footerContentLeft}
+                        </div>
+                        <div className={"right_col"}>
+                            {footerContent}
+                        </div>
+                    </div>
+                }
+
+                {
+                    (!footerContentLeft && footerContent) &&
+                    < div className={"card_footer_right"}>
+                        {footerContent}
                     </div>
                 }
             </div>
-            <div className="subtitle_and_text">
-                {subtitle &&
-                    <p className="card_subtitle">{subtitle}</p>
-                }
-                <div className="card_text">
-                    {text}
-                </div>
-            </div>
 
 
-            {/* Card Footer, typically contains primary/secondary buttons */}
-            {
-                (footerContentLeft && footerContent) &&
-                <div className={"card_footer_right_and_left"}>
-                    {footerContentLeft}
-                    {footerContent}
-                </div>
-            }
 
-            {
-                (!footerContentLeft && footerContent) &&
-                < div className={"card_footer_right"}>
-                    {footerContent}
-                </div>
-            }
 
 
         </div>
@@ -100,7 +104,7 @@ HorizontalCard.propTypes = {
     imageAlt: PropTypes.string,
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string,
-    text: PropTypes.object,
+    text: PropTypes.string,
     callback: PropTypes.func,
     upperRightContent: PropTypes.any,
     footerContentLeft: PropTypes.any,
