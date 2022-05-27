3236cc38df6cf5524e39d2f8f649e8c9e03c7af2
Andrea Locatelli <ciao.loca@gmail.com>
2022-05-27 12:21:39 +0200

    File: LayoutBackOfficem Dashboard -> add dashboard

--- src/screens/backOffice/host/dashboard/Dashboard.js
+++ src/screens/backOffice/host/dashboard/Dashboard.js
@@ -1,30 +1,91 @@
-import { Card, Col, Row } from "antd";
+import { Card, Col, Row, Button } from "antd";
 
-const App = () => (
-  <div className="site-card-wrapper">
-    <Row gutter={16}>
-      <Col span={8}>
-        <Card title="Structure" bordered={false}>
-          Manage your structure
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faWallet,
+  faBuilding,
+  faBook,
+  faMessage,
+} from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import { routes } from "../../../../routes/routes";
+
+const Dashboard = () => {
+  const navigate = useNavigate();
+
+
+  const goTo = (path, obj = null) => () => {
+    return navigate(`/${routes.DASHBOARD}/${path}`)
+  }
+
+  return (
+    <Row gutter={[48, 16]} justify="space-evenly" align="middle">
+      <Col span={12}>
+        <Card
+          title={
+            <span>
+              <FontAwesomeIcon icon={faBuilding} /> Structure
+            </span>
+          }
+          bordered={true}
+          onClick={goTo(routes.STRUCTURE_LIST)}
+          style={{
+            cursor: "pointer",
+          }}
+        >
+          <p>Manage your structure</p>
         </Card>
       </Col>
-      <Col span={8}>
-        <Card title="Bookings" bordered={false}>
-          Manage your bookings
+      <Col span={12}>
+        <Card
+          title={
+            <span>
+              <FontAwesomeIcon icon={faBook} /> Booking
+            </span>
+          }
+          bordered={false}
+          onClick={goTo(routes.RESERVATION_LIST)}
+          style={{
+            cursor: "pointer",
+          }}
+        >
+          <p>Manage your booking</p>
+        </Card>
+      </Col>
+
+      <Col span={12}>
+        <Card
+          title={
+            <span>
+              <FontAwesomeIcon icon={faMessage} /> Messages
+            </span>
+          }
+          bordered={true}
+          onClick={goTo(routes.MESSAGE_LIST)}
+          style={{
+            cursor: "pointer",
+          }}
+        >
+          <p>Manage message</p>
+        </Card>
+      </Col>
+      <Col span={12}>
+        <Card
+          title={
+            <span>
+              <FontAwesomeIcon icon={faWallet} /> Payments
+            </span>
+          }
+          bordered={false}
+          style={{
+            cursor: "pointer",
+          }}
+        >
+          <p>Manage payments</p>
         </Card>
       </Col>
     </Row>
-    <Row>
-      <Col span={8}>
-        <Card title="Massages" bordered={false}>
-          Answer all the question
-        </Card>
-        <Card title="Payments" bordered={false}>
-          Manage the wallet
-        </Card>
-      </Col>
-    </Row>
-  </div>
-);
+  );
+};
 
-export default App;
+export default Dashboard;
